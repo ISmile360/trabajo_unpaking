64e88595ab2f565d675721d971fd87495b337ada
KaiZeRx360 <kaizerx360@gmail.com>
2025-03-14 09:45:15 -0600

    Segundo commit

---
+++ trabajo2.py
@@ -0,0 +1,7 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+a,*c,b = numeros
+
+print(f"Lista normal: {numeros}")
+print(f"Letra A primer numero: {a}")
+print(f"Letra B ultimo numero: {b}")
+print(f"Letra C numeros intermedios: {c}")
